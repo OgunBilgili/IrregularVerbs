afee11e6674618cdb3dee70ed4a1e7b9f564a7dc
Ogun Bilgili <ogunbilgili@gmail.com>
2021-07-15 18:23:39 +0300

    Session Page has been renewed.

---
+++ IrregularVerbs/ViewModels/Feedback.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IrregularVerbs.ViewModels
+{
+    public class Feedback
+    {
+         
+    }
+}
